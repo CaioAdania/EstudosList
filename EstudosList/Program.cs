96c1a28a801b382504388b461e89ed6b75f045e2
CaioAdania <caioadania@gmail.com>
2024-06-12 11:43:35 -0300

    estudos list

---
+++ EstudosList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace EstudosList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            List<string> list = new List<string>();
+
+            List<string> list2 = new List<string>() { "Maria", "Alex" }; //outra forma de fazer
+
+
+
+        }
+    }
+}
